926d05d7e57df51d0df454ffec5926614d396d9f
nitsas <christosn654@gmail.com>
2014-11-06 15:40:06 +0200

    [quicksort] Change notation, use word "list" instead of "sequence"

--- quicksort.py
+++ quicksort.py
@@ -1,10 +1,10 @@
 """
-Sort a sequence of items using the quicksort algorithm.
+Sort a list of items using the quicksort algorithm.
 
 The default is to choose pivots uniformly at random. This can be overriden
 though, by setting the choose_pivot module attribute to some other preferred
-function with signature choose(sequence, index_l, index_r), which chooses one
-of the items sequence[index_l:index_r] as the pivot and returns its index,
+function with signature choose(list_, index_l, index_r), which chooses one
+of the items list_[index_l:index_r] as the pivot and returns its index,
 index_p, where index_l <= index_p < index_r.
 
 Author:
@@ -27,17 +27,16 @@ __all__ = ['sort', 'quicksort', 'partition', 'choose_pivot_index',
            'choose_random_pivot_index']
 
 
-def quicksort(sequence, begin=None, end=None):
+def quicksort(list_, begin=None, end=None):
     """
-    Sort the items in the given (part of the) sequence in place.
+    Sort the items in the given (part of the) list in place.
 
-    sequence -- A sequence of items that can be compared using the less than
-                or equal (<=) operator. We assume that the sequence is
-                indexable.
+    list_ -- A list of items that can be compared using the less than
+             or equal (<=) operator.
     begin -- The index of the first of the items that we want to sort.
              Default 0. (0 <= begin <= end must hold)
     end -- The index just past the last of the items that we want to sort.
-           Default len(sequence). (begin <= end <= len(sequence) must hold)
+           Default len(list_). (begin <= end <= len(list_) must hold)
 
     This does NOT produce a stable sort. A stable sort is one where, if two
     items compare equal their relative order is preserved, so that if one came
@@ -48,23 +47,23 @@ def quicksort(sequence, begin=None, end=None):
     if begin is None:
         begin = 0
     if end is None:
-        end = len(sequence)
+        end = len(list_)
     # check for invalid indices:
-    if not 0 <= begin <= end <= len(sequence):
-        raise ValueError("0 <= begin <= end <= len(sequence) must hold")
-    _quicksort_recurse(sequence, begin, end)
+    if not 0 <= begin <= end <= len(list_):
+        raise ValueError("0 <= begin <= end <= len(list_) must hold")
+    _quicksort_recurse(list_, begin, end)
     return
 
 
 sort = quicksort
 
 
-def _quicksort_recurse(sequence, begin, end):
+def _quicksort_recurse(list_, begin, end):
     """
-    Sort the items in the given (part of the) sequence in place. (helper)
+    Sort the items in the given (part of the) list in place. (helper)
 
-    sequence -- a sequence of items that can be compared using the less than
-                or equal (<=) operators
+    list_ -- a list of items that can be compared using the less than
+             or equal (<=) operators
     begin -- the index of the leftmost item we care about
     end -- one past the index of the rightmost item we care about
     """
@@ -72,32 +71,32 @@ def _quicksort_recurse(sequence, begin, end):
     if end - begin <= 1:
         return
     # choose pivot index
-    index_p = choose_pivot_index(sequence, begin, end)
+    index_p = choose_pivot_index(list_, begin, end)
     # partition around the pivot and get the new index of the pivot
-    index_p = partition(sequence, begin, end, index_p)
+    index_p = partition(list_, begin, end, index_p)
     # recurse on the left and right *halves*
-    _quicksort_recurse(sequence, begin, index_p)
-    _quicksort_recurse(sequence, index_p + 1, end)
+    _quicksort_recurse(list_, begin, index_p)
+    _quicksort_recurse(list_, index_p + 1, end)
     return
 
 
-def partition(seq, begin, end, index_p):
+def partition(list_, begin, end, index_p):
     """
     Partition around the pivot and return the final index of the pivot.
 
-    seq -- a sequence of items that can be compared using the less than or
-           equal (<=) operator
+    list_ -- a list of items that can be compared using the less than or
+             equal (<=) operator
     begin -- the index of the leftmost item we care about
     end -- one past the index of the rightmost item we care about
     index_p -- the (starting) index of the pivot
 
-    When this function returns with result index_p, the original subsequence
-    will be partitioned so that the items sequence[index_l:index_p] will be
-    less than the pivot, and the items sequence[index_p+1:index_r]
-    will be greater than the pivot.
+    When this function returns with result index_p, the original sublist will
+    be partitioned so that the items list_[index_l:index_p] will be less than
+    the pivot, and the items list_[index_p+1:index_r] will be greater than the
+    pivot.
 
     TODO:
-    Won't this lead to quadratic behavior in sequences which contain only a
+    Won't this lead to quadratic behavior in lists which contain only a
     single value multiple times? Everything will always compare less than or
     equal to any pivot. I can solve this by implementing a three-way "fat"
     partition, i.e. partition the elements into three groups, the elements
@@ -105,18 +104,18 @@ def partition(seq, begin, end, index_p):
     than the pivot.
     """
     # the pivot (for readability)
-    pivot = seq[index_p]
+    pivot = list_[index_p]
     # temporarily move the pivot to the end
-    seq[end - 1], seq[index_p] = seq[index_p], seq[end - 1]
+    list_[end - 1], list_[index_p] = list_[index_p], list_[end - 1]
     # keep a separator pointing to the first item that might be
     # greater than the pivot
     separator = begin
     # compare each item to the pivot and move either the item
     # or the separator accordingly
     for i in range(begin, end - 1):
-        if seq[i] <= pivot:
+        if list_[i] <= pivot:
             # move it to where the separator is and advance the separator
-            seq[separator], seq[i] = seq[i], seq[separator]
+            list_[separator], list_[i] = list_[i], list_[separator]
             separator += 1
         # else just advance i
     # this is the current state:
@@ -124,19 +123,19 @@ def partition(seq, begin, end, index_p):
     # - the separator points to the first item that is greater than the pivot
     # - everything to the right of the separator (except the last position) is
     # greater than the pivot
-    # - the pivot is at the last position (end of the subsequence)
+    # - the pivot is at the last position (end of the sublist)
     # swap pivot and separator
-    seq[separator], seq[end - 1] = seq[end - 1], seq[separator]
+    list_[separator], list_[end - 1] = list_[end - 1], list_[separator]
     # the separator now points to (i.e. is the index of) the pivot
     return separator
 
 
-def choose_random_pivot_index(sequence, begin, end):
+def choose_random_pivot_index(list_, begin, end):
     """
     Choose a pivot uniformly at random and return its index.
 
-    sequence -- a sequence of items that can be compared using the less than
-                or equal operator (<=)
+    list_ -- a list of items that can be compared using the less than
+             or equal operator (<=)
     begin -- the index of the leftmost item we care about
     end -- one past the index of the rightmost item we care about
     """
