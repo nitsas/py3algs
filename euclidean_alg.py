eaeace354a024b8d64ec0f6d2a76b8a97d5ff571
nitsas <nitsas.chris@gmail.com>
2014-11-15 19:25:16 +0200

    [euclidean_alg] Add comments inside extended_gcd

--- euclidean_alg.py
+++ euclidean_alg.py
@@ -52,15 +52,22 @@ def extended_gcd(a, b):
     a -- an integer (isinstance(a, numbers.Integral) must hold)
     b -- an integer (isinstance(a, numbers.Integral) must hold)
     """
+    
+    # input must be integers
     if not isinstance(a, numbers.Integral) or \
        not isinstance(b, numbers.Integral):
         raise TypeError("Can't find gcd of non-integral objects '" + \
                         a.__class__.__name__ + "' and '" + \
                         b.__class__.__name__ + "'")
     
+    # make the necessary conversions so that we only need to solve 
+    # the case where: a >= b >= 0
     if not (a >= b >= 0):
         aa, bb = max(abs(a), abs(b)), min(abs(a), abs(b))
+        # call this recursively with aa and bb, where: aa >= bb >= 0
+        # don't worry, we'll get at most 1 recursive call
         gcd, s, t = extended_gcd(aa, bb)
+        # figure out the result for (a, b) from the result for (aa, bb)
         if abs(b) >= abs(a):
             s, t = t, s
         if a < 0:
@@ -71,11 +78,14 @@ def extended_gcd(a, b):
     
     assert(a >= b >= 0)
     
+    # we'll mutate a, b, remember them if you'll use the assertion below
     orig_a, orig_b = a, b
     
+    # initialization
     old_s, s = 1, 0
     old_t, t = 0, 1
     
+    # main loop
     while b != 0:
         k = a//b
         r = a%b
@@ -86,6 +96,7 @@ def extended_gcd(a, b):
     
     # when the current a was r, the current old_s and old_t were s and t
     # and they all satisfied: r == orig_a*s + orig_b*t
+    # they are the solution we were looking for
     gcd, s, t = a, old_s, old_t
     
     return gcd, s, t
