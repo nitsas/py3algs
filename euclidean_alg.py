6be7fad779b6e0b0f8fe3aaf2056cffe9281f009
nitsas <nitsas.chris@gmail.com>
2014-11-15 19:12:54 +0200

    [euclidean_alg] Add the extended Euclidean algorithm

---
+++ euclidean_alg.py
@@ -0,0 +1,91 @@
+"""
+Compute the greatest common divisor (gcd) of a pair of integers.
+
+Includes the extended Euclidean algorithm for finding the gcd.
+
+Author:
+  Christos Nitsas
+  (nitsas)
+  (chrisnitsas)
+
+Language:
+  Python 3(.3)
+
+Date:
+  March, 2014
+"""
+
+
+import numbers
+
+
+__all__ = ['extended_gcd']
+
+
+def gcd(a, b):
+    """
+    Computes the greatest common divisor of integers a, b and returns an int.
+    
+    input parameters:
+    a -- an integer (isinstance(a, numbers.Integral) must hold)
+    b -- an integer (isinstance(a, numbers.Integral) must hold)
+    """
+    if not isinstance(a, numbers.Integral) or \
+       not isinstance(b, numbers.Integral):
+        raise TypeError("Can't find gcd of non-integral objects '" + \
+                        a.__class__.__name__ + "' and '" + \
+                        b.__class__.__name__ + "'")
+    raise NotImplementedError()
+
+
+def extended_gcd(a, b):
+    """
+    Computes the greatest common divisor (gcd) of integers a, b plus a pair of 
+    Bezout's coefficients s, t and returns the tuple (gcd, s, t).
+    
+    A pair s, t of Bezout's coefficients are integers that, together with the
+    input parameters a, b, satisfy the equation:
+    a * s + b * t = gcd
+    In fact, this returns one minimum such pair.
+    
+    input parameters:
+    a -- an integer (isinstance(a, numbers.Integral) must hold)
+    b -- an integer (isinstance(a, numbers.Integral) must hold)
+    """
+    if not isinstance(a, numbers.Integral) or \
+       not isinstance(b, numbers.Integral):
+        raise TypeError("Can't find gcd of non-integral objects '" + \
+                        a.__class__.__name__ + "' and '" + \
+                        b.__class__.__name__ + "'")
+    
+    if not (a >= b >= 0):
+        aa, bb = max(abs(a), abs(b)), min(abs(a), abs(b))
+        gcd, s, t = extended_gcd(aa, bb)
+        if abs(b) >= abs(a):
+            s, t = t, s
+        if a < 0:
+            s = -s
+        if b < 0:
+            t = -t
+        return gcd, s, t
+    
+    assert(a >= b >= 0)
+    
+    orig_a, orig_b = a, b
+    
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+    
+    while b != 0:
+        k = a//b
+        r = a%b
+        old_s, s = s, old_s - k*s
+        old_t, t = t, old_t - k*t
+        assert(r == orig_a*s + orig_b*t)
+        a, b = b, r
+    
+    # when the current a was r, the current old_s and old_t were s and t
+    # and they all satisfied: r == orig_a*s + orig_b*t
+    gcd, s, t = a, old_s, old_t
+    
+    return gcd, s, t
