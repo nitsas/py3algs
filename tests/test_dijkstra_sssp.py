b302bacad6aecc87fb5c0b004bc186561533cf52
nitsas <nitsas.chris@gmail.com>
2014-11-21 19:19:03 +0200

    [test_dijkstra_sssp] Fix test on not-connected graph

--- tests/test_dijkstra_sssp.py
+++ tests/test_dijkstra_sssp.py
@@ -14,7 +14,7 @@ def make_graph(num_nodes, num_edges, seed=None, directed=True):
     # create the graph 
     # (if we use the same seed every time, we'll get the same graph)
     graph = nx.gnm_random_graph(num_nodes, num_edges, seed=seed,
-                                directed=True)
+                                directed=directed)
     # assign edge weights
     # (again we use a seed so we get the same weights every time)
     random.seed(seed)
@@ -67,7 +67,7 @@ class DijkstraSsspOnUndirectedNotConnectedGraph(unittest.TestCase):
     
     def setUp(self):
         num_nodes = 200
-        num_edges = 800
+        num_edges = 400
         seed = 0
         # make the graph
         self.graph = make_graph(num_nodes, num_edges, seed, directed=False)
@@ -79,8 +79,15 @@ class DijkstraSsspOnUndirectedNotConnectedGraph(unittest.TestCase):
         dist, _ = dijkstra_sssp.solve(self.graph, self.source, weight='weight')
         nx_dist = nx.single_source_dijkstra_path_length(self.graph,
                                             self.source, weight='weight')
+        inf = float('inf')
         for node in self.graph.nodes_iter():
-            self.assertEqual(dist[node], nx_dist[node])
+            if dist[node] != inf:
+                # node was reachable
+                self.assertEqual(dist[node], nx_dist[node])
+            else:
+                # node was unreachable
+                self.assertTrue(node not in nx_dist)
+                self.assertEqual(dist[node], inf)
 
 
 def main():
