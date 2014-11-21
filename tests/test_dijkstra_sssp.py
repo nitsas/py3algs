efc06f7275b3c2596dae04c1f949e693e8171c6a
nitsas <nitsas.chris@gmail.com>
2014-11-21 23:55:58 +0200

    [test_dijkstra_sssp] Merged the two test cases into one

--- tests/test_dijkstra_sssp.py
+++ tests/test_dijkstra_sssp.py
@@ -21,34 +21,38 @@ def make_graph(num_nodes, num_edges, seed=None, directed=True):
     for _, _, edge_attrs in graph.edges_iter(data=True):
         edge_attrs['weight'] = random.randint(0, 99)
     return graph
-    
-class DijkstraSsspOnDirectedStronglyConnectedGraphTestCase(unittest.TestCase):
+
+
+class DijkstraSsspTestCase(unittest.TestCase):
     """
-    Test dijkstra_sssp against networkx's dijkstra on a small directed
-    strongly connectedgraph.
+    Test dijkstra_sssp against networkx's dijkstra implementation.
     
-    Will test against networkx.single_source_dijkstra_path_length on a 
-    directed and strongly connected networkx graph created using 
-    networkx.gnm_random_graph with the same seed every time. The edge weights 
-    are drawn using random.randint(0, 99), again with the same seed every time.
+    Will test against networkx.single_source_dijkstra_path_length on graphs
+    we create using a random graph generator (networkx.gnm_random_graph) but 
+    with the same seed every time. The edge weights are drawn using 
+    random.randint(0, 99), again with the same seed every time. This means
+    each time test_A is run it will always get graph_A to work on.
+    
+    Different tests will have different graphs though.
     """
     
-    def setUp(self):
-        num_nodes = 200
-        num_edges = 1800
-        seed = 0
-        # make the graph
-        self.graph = make_graph(num_nodes, num_edges, seed)
-        assert(nx.is_strongly_connected(self.graph))
-        # the source node
-        self.source = 0
-    
-    def test_vs_networkx_single_source_dijkstra(self):
-        dist, _ = dijkstra_sssp.solve(self.graph, self.source, weight='weight')
-        nx_dist = nx.single_source_dijkstra_path_length(self.graph,
-                                            self.source, weight='weight')
+    def test_on_directed_strongly_connected_graph(self):
+        """
+        Test on a small directed strongly connected graph.
+        """
+        # make the strongly connected directed graph:
+        params_dscg = {'num_nodes': 200, 'num_edges': 1800, 'seed': 0, 
+                       'directed': True}
+        graph = make_graph(**params_dscg)
+        assert(nx.is_strongly_connected(graph))
+        # first run dijkstra_sssp
+        dist, _ = dijkstra_sssp.solve(graph, source=0, weight='weight')
+        # then networkx's dijkstra
+        nx_dist = nx.single_source_dijkstra_path_length(graph, source=0, 
+                                                        weight='weight')
+        # finally, compare results
         inf = float('inf')
-        for node in self.graph.nodes_iter():
+        for node in graph.nodes_iter():
             if dist[node] != inf:
                 # node was reachable
                 self.assertEqual(dist[node], nx_dist[node])
@@ -56,38 +60,26 @@ class DijkstraSsspOnDirectedStronglyConnectedGraphTestCase(unittest.TestCase):
                 # node was unreachable
                 self.assertTrue(node not in nx_dist)
                 self.assertEqual(dist[node], inf)
-
-
-class DijkstraSsspOnUndirectedNotConnectedGraphTestCase(unittest.TestCase):
-    """
-    Test dijkstra_sssp against networkx's dijkstra on an undirected and not
-    connected graph. 
     
-    Will test against networkx.single_source_dijkstra_path_length on an 
-    undirected and not connected networkx graph created using 
-    networkx.gnm_random_graph. The edge weights are drawn using 
-    random.randint(0, 99). We use a constant seed for both random generators,
-    the same seed every time this is run, so we'll always get the same graph.
-    
-    Not all nodes will be reachable from the source.
-    """
-    
-    def setUp(self):
-        num_nodes = 200
-        num_edges = 400
-        seed = 0
-        # make the graph
-        self.graph = make_graph(num_nodes, num_edges, seed, directed=False)
-        assert(not nx.is_connected(self.graph))
-        # the source node
-        self.source = 0
-    
-    def test_vs_networkx_single_source_dijkstra(self):
-        dist, _ = dijkstra_sssp.solve(self.graph, self.source, weight='weight')
-        nx_dist = nx.single_source_dijkstra_path_length(self.graph,
-                                            self.source, weight='weight')
+    def test_on_undirected_not_connected_graph(self):
+        """
+        Test on an undirected and not connected graph. 
+        
+        Not all nodes will be reachable from the source.
+        """
+        # make the undirected not connected graph
+        params_uncg = {'num_nodes': 200, 'num_edges': 400, 'seed': 0, 
+                       'directed': False}
+        graph = make_graph(**params_uncg)
+        assert(not nx.is_connected(graph))
+        # first run dijkstra_sssp 
+        dist, _ = dijkstra_sssp.solve(graph, source=0, weight='weight')
+        # then run networkx's dijkstra
+        nx_dist = nx.single_source_dijkstra_path_length(graph, source=0, 
+                                                        weight='weight')
+        # finally, compare results
         inf = float('inf')
-        for node in self.graph.nodes_iter():
+        for node in graph.nodes_iter():
             if dist[node] != inf:
                 # node was reachable
                 self.assertEqual(dist[node], nx_dist[node])
