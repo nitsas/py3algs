7f3e8ee72d5184caa20b6a7f0699f8e63c8d9f28
nitsas <nitsas.chris@gmail.com>
2014-11-21 16:58:42 +0200

    [johnsons_apsp] Add parameter to choose edge attr to use as a weight
    Add the `weight` parameter with default value 'weight' (previously we used
    'cost' as a hardcoded default), so that users can choose which edge attribute
    to use as edge weights.

--- johnsons_apsp.py
+++ johnsons_apsp.py
@@ -35,12 +35,14 @@ DistAndPred = collections.namedtuple('DistAndPred', ['dist', 'pred'])
 NegativeCycleError = bellman_ford_sssp.NegativeCycleError
 
 
-def _add_supersource(graph):
+def _add_supersource(graph, edge_weight_attr='weight'):
     """
-    This will add a new "supersource" node to the graph as well 
-    as zero cost edges from the supersource to every other node.
+    This will add a new "supersource" node to the graph as well as 
+    zero-weight edges from the supersource to every other node.
     
     graph -- a networkx graph
+    edge_weight_attr -- the name of the edge attribute we'll use as edge 
+                        weights (default: 'weight')
     """
     ss = '_temp_supersource'
     if ss in graph:
@@ -50,25 +52,28 @@ def _add_supersource(graph):
             raise Exception('Failed to add temporary supersource.')
     graph.add_node(ss)
     for node in graph.nodes_iter():
-        graph.add_edge(ss, node, cost=0)
+        graph.add_edge(ss, node, {edge_weight_attr: 0})
     graph.remove_edge(ss, ss)
     return ss
 
 
-def _compute_node_weights(graph):
+def _compute_node_weights(graph, edge_weight_attr='weight'):
     """
     Compute a weight for each node, such that reweighting each edge (u, v) 
     using the formula: 
-    cost'(u, v) = cost(u, v) + weight(u) - weight(v)
-    will result in non-negative edge costs.
+    weight'(u, v) = weight(u, v) + weight(u) - weight(v)
+    will result in non-negative edge weights.
     
     graph -- a networkx graph
+    edge_weight_attr -- the name of the edge attribute we'll use as edge 
+                        weights (default: 'weight')
     """
     # add a supersource
-    ss = _add_supersource(graph)
+    ss = _add_supersource(graph, edge_weight_attr)
     # compute distances from the supersource; 
     # those will become node weights
-    node_weight, node_pred = bellman_ford_sssp.solve(graph, ss)
+    node_weight, node_pred = bellman_ford_sssp.solve(graph, ss, 
+                                                     weight=edge_weight_attr)
     # remove the supersource
     graph.remove_node(ss)
     del(node_weight[ss])
@@ -76,36 +81,36 @@ def _compute_node_weights(graph):
 
 
 def _reweight_edges_using_node_weights(graph, node_weight, 
-                                     weight_attr_name=None):
+                                       edge_weight_attr='weight'):
     """
     Reweight each edge (u, v) using the formula:
-    cost'[(u, v)] = cost[(u, v)] + node_weight[u] - node_weight[v]
+    weight'[(u, v)] = weight[(u, v)] + node_weight[u] - node_weight[v]
+    
     This will preserve shortest paths.
     
     graph -- a networkx graph
     node_weight -- a dictionary mapping nodes to weights
-    weight_attr_name -- the name of the edge attribute we want to 
-                        use as weights/costs (default: 'cost')
+    edge_weight_attr -- the name of the edge attribute we'll use as edge 
+                        weights (default: 'weight')
     
     For suitable node weights, this will get us non-negative edge 
     weights.
     """
     # set name of edge weight attribute
-    if weight_attr_name is None:
-        weight = 'cost'
-    else:
-        weight = weight_attr_name
-    # use the node weights to assign a new (non-negative) cost 
-    # to each edge which preserves shortest paths 
+    # use the node weights to assign a new (non-negative) weight to each 
+    # edge, while preserving shortest paths 
     for u, v, attrs in graph.edges_iter(data=True):
-        attrs[weight] = attrs[weight] + node_weight[u] - node_weight[v]
+        attrs[edge_weight_attr] = attrs[edge_weight_attr] + \
+                                  node_weight[u] - node_weight[v]
 
 
-def johnsons_all_pairs_shortest_paths(graph_):
+def johnsons_all_pairs_shortest_paths(graph_, weight='weight'):
     """
     Compute a shortest path for every pair of nodes in the graph.
     
     graph_ -- a networkx graph
+    weight -- the name of the edge attribute we'll use as edge weights 
+              (default: 'weight')
     
     Return a namedtuple of two dictionaries (dist, pred). Both dist and pred
     map each node (source) in the graph to another dict, which in turn maps
@@ -122,16 +127,18 @@ def johnsons_all_pairs_shortest_paths(graph_):
     # compute node weights, for the edge reweighting step
     # (this will throw a NegativeCycleError exception if the graph 
     # contains a negative cycle)
-    node_weight = _compute_node_weights(graph)
+    node_weight = _compute_node_weights(graph, edge_weight_attr=weight)
     # reweigh all the graph's edges so that there are no negative edges
     # this operarion preserves shortest paths
-    _reweight_edges_using_node_weights(graph, node_weight)
+    _reweight_edges_using_node_weights(graph, node_weight, 
+                                       edge_weight_attr=weight)
     # call dijkstra's single source shortest paths algorithm once 
     # for each node in the graph, to compute all pairs shortest paths
     dist, pred = dict(), dict()
     for node in graph.nodes_iter():
         # compute shortest paths using the current node as a source
-        dist[node], pred[node] = dijkstra_sssp.solve(graph, node)
+        dist[node], pred[node] = dijkstra_sssp.solve(graph, node,
+                                                     weight=weight)
     # remember that we have reweighted the edges; the actual shortest 
     # paths didn't change but their lengths did, so we now have to 
     # undo the effects of reweighting in the shortest path lengths 
