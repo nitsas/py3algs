9005338e6589a11d67ce7d1d041af2cc81a6711a
nitsas <nitsas.chris@gmail.com>
2015-01-06 21:14:29 +0200

    Make this into a package (currently algorithms only)

---
+++ py3algs/__init__.py
@@ -0,0 +1,4 @@
+from . import algorithms
+from .algorithms import *
+
+__all__ = algorithms.__all__
---
+++ py3algs/algorithms/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['bellman_ford_sssp', 'binary_exponentiation', 'binary_search', \
+           'count_inversions', 'dijkstra_sssp', 'diophantine', \
+           'euclidean_alg', 'johnsons_apsp', 'karger_min_cut', \
+           'knapsack', 'kosaraju_scc', 'quickselect', 'quicksort', \
+           'tsp', 'two_three_sum']
+
+from . import *
